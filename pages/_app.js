f4c06f86e4ad52f819dab2ad0394edd1f0293a92
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-05 20:50:50 -0300

    reparando-ssr-material-ui-estilos

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //--- Corregir error de material-ui ---
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  //--- Fin Corregir error de material-ui ---
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
