10e697ce6b7876710d7d75c5cf64488943fe3cc7
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-07 17:05:48 -0300

    Actualizacion-Borrado de Productos Carrito

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,31 @@ import {
   Button,
 } from "@material-ui/core";
 import Layout from "../components/Layout";
-//import data from "../utils/data";
 import db from "../utils/db";
 import Product from "../models/Product";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { Store } from "../utils/Store";
 import NextLink from "next/link";
 
 export default function Home(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
   const { products } = props;
+
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+    router.push("/cart");
+  };
+
   return (
     <Layout>
       <div>
@@ -39,7 +57,11 @@ export default function Home(props) {
 
                 <CardActions>
                   <Typography>$ {product.price}</Typography>
-                  <Button size="small" color="primary">
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => addToCartHandler(product)}
+                  >
                     Add to cart
                   </Button>
                 </CardActions>
