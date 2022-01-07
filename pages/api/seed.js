ff31486ed0198ed41874bcd15f18dd9549f328e7
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-07 18:17:41 -0300

    Insertando Usuarios de Prueba

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,12 +1,15 @@
-import nextConnect from "next-connect";
+import nc from "next-connect";
 import Product from "../../models/Product";
 import data from "../../utils/data";
 import db from "../../utils/db";
+import User from "../../models/User";
 
-const handler = nextConnect();
+const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
   await Product.deleteMany();
   await Product.insertMany(data.products);
   await db.disconnect();
