97df2019ad178427100b6e08b66ab2c6008d7fe8
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-07 22:14:03 -0300

    Validando Usuario-Login

--- pages/login.js
+++ pages/login.js
@@ -6,15 +6,33 @@ import {
   Button,
   Link,
 } from "@material-ui/core";
+import axios from "axios";
 import NextLink from "next/link";
 import Layout from "../components/Layout";
 import useStyles from "../utils/styles";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
+      alert("success login");
+    } catch (error) {
+      alert(error.response.data ? error.response.data.message : error.message);
+    }
+  };
+
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -26,6 +44,7 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: "email" }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -35,6 +54,7 @@ export default function Login() {
               id="password"
               label="Password"
               inputProps={{ type: "password" }}
+              onChange={(e)=> setPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
